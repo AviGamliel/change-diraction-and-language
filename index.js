d553082bcb3505621291183697a91e1cb6386a50
AviGamliel <56459192+AviGamliel@users.noreply.github.com>
2020-12-08 20:28:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+(function(){
+  console.log('bookmark starting');
+let paragraphs = document.getElementsByTagName('p');
+  for(var i =0; i<paragraphs.length; i++){
+    paragraphs[i].innerHTML = 'שלאקות לפנים !!';
+}),();
